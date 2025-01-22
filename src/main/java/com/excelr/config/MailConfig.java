1668be44bb904442682c26457f004c18f5b8a7fd
Gowtham Sannapaneni <gowthamtechiee@gmail.com>
2025-01-22 10:25:30 +0530

    Update MailConfig.java

--- src/main/java/com/excelr/config/MailConfig.java
+++ src/main/java/com/excelr/config/MailConfig.java
@@ -13,8 +13,8 @@ public class MailConfig {
         JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
         mailSender.setHost("smtp.gmail.com");
         mailSender.setPort(587);
-        mailSender.setUsername("gadgetrush.notify@gmail.com");
-        mailSender.setPassword("bzglftgfdvfludkd");
+        mailSender.setUsername("Mail");
+        mailSender.setPassword("Pass");
         
         Properties props = mailSender.getJavaMailProperties();
         props.put("mail.smtp.auth", "true");
